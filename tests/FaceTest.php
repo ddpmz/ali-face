ee69962efeafbe9444bf7ac1108af3409c78b1a3
renfan <renfan1204@qq.com>
2019-04-08 13:26:10 +0000

    Apply fixes from StyleCI

--- tests/FaceTest.php
+++ tests/FaceTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the renfan/face.
+ *
+ * (c) renfan <renfan1204@qq.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Renfan\Face\Tests;
 
 use PHPUnit\Framework\TestCase;
