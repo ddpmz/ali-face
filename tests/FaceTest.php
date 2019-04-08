70f27c51fe10b19bf766547ae4b059e856fa9bac
renfan <renfan1204@qq.com>
2019-04-08 12:45:50 +0000

    Apply fixes from StyleCI

--- tests/FaceTest.php
+++ tests/FaceTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the renfan/face.
+ *
+ * (c) renfan <renfan1204@qq.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Renfan\Face\Tests;
 
 use PHPUnit\Framework\TestCase;
@@ -17,7 +26,8 @@ class FaceTest extends TestCase
         $this->fail('Failed to assert verify throw exception with invalid argument.');
     }
 
-    public function testAliApiAccessWithInvalidPath() {
+    public function testAliApiAccessWithInvalidPath()
+    {
         $f = new Face('mock-key', 'mock-key');
         $this->expectException(InvalidArgumentException::class);
         $this->expectExceptionMessage('Invalid type value(detect, attribute, verify): foo');
